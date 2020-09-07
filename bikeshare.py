5b47fb4c18894f83594ef8e181ef35400079af4a
Romanskyy Andrey <andrey.romanskyy@icloud.com>
2020-09-07 22:51:09 +0300

    Make changes in document string for get_filters function, bikeshare.py
    Add range of months

--- bikeshare.py
+++ bikeshare.py
@@ -15,7 +15,7 @@ def get_filters():
 
     Returns:
         (str) city - name of the city to analyze
-        (str) month - name of the month to filter by, or "all" to apply no month filter
+        (str) month - name of the month (from january till june) to filter by, or "all" to apply no month filter
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
 
